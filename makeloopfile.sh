84664153c4b75e401c2964927c6233cd82348ef9
CSNguyenT <tommy.1115@revature.net>
2021-11-17 10:51:44 -0500

    Cleaned out unneeded quotation marks and simplified filepath into a variable.

--- makeloopfile.sh
+++ makeloopfile.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/bash
 
+# shortened designated filepath for easier use
+filepath=./loopresult/loopfile.txt
+
 mkdir ./loopresult
-touch ./loopresult/loopfile.txt
-echo "Results from makeloopfile.sh file" > ./loopresult/loopfile.txt
-for i in {1..20..2}
-do
-    echo "$i" >> ./loopresult/loopfile.txt
+touch $filepath
+echo "Results from makeloopfile.sh file" > $filepath
+for i in {1..20..2}; do
+	echo $i >> $filepath
 done
 echo ""
-for i in {2..20..2}
-do
-    echo -n "$i" >> ./loopresult/loopfile.txt
-	if [ "$i" -lt "20" ]; then
-		echo -n ", " >> ./loopresult/loopfile.txt
+for i in {2..20..2}; do
+    echo -n $i >> $filepath
+	if [ $i -lt 20 ]; then
+		echo -n ", " >> $filepath
 	fi
 done
--- upload.sh
+++ upload.sh
@@ -6,11 +6,11 @@ cwd=$(pwd)
 addfile="Y"
 
 #this loop is for asking to add more files to push
-while [ "$addfile" = "y" ] || [ "$addfile" = "Y" ]; do
+while [ $addfile = "y" ] || [ $addfile = "Y" ]; do
 	# --ask to select the file(s) in the working directory--
 	echo "Which file(s) should be uploaded in this directory?"
-	echo "Your current working directory: $cwd"
-	echo "Your files in this working directory:"
+	echo "Current working directory: $cwd"
+	echo "Files in this working directory:"
 	ls
 	read file
 	
@@ -18,32 +18,31 @@ while [ "$addfile" = "y" ] || [ "$addfile" = "Y" ]; do
 	if [ "${file: -1}" == "." ]; then
 		echo "Are you sure you want to upload all files in that directory? Type 'y' to allow."
 		read allow
-		if [ "$allow" == "y" ] || [ "$allow" == "Y" ]; then
+		if [ $allow == "y" ] || [ $allow == "Y" ]; then
 			git add "$file"
 		fi
 	elif [ -z "$file" ]; then
 		echo "There is no input."
-		allow="N"
-		
+		allow=N
 	else
 		git add "$file"
-		allow="Y"
+		allow=Y
 	fi
 	
 	# allow to read if file has been added
-	if [ "$allow" == "y" ] || [ "$allow" == "Y" ]; then
+	if [ $allow == "y" ] || [ $allow == Y ]; then
 		echo "Attempted to add $file to the index."
 	fi
-	echo "Would you like to add another file? Type 'y' to add another file."
+	echo "Do you want to add another file? Type 'y' to add another file."
 	read addfile
 done
 
 #commit and push
-echo "Write a commit for the changes to be made."
+echo "Write a commit for the changes made to the files."
 read message
 echo "type 'y' to push these files for change or type anything else to cancel."
 read uploadfile
-if [ "$uploadfile" == "y" ] || [ "$uploadfile" == "Y" ]; then
+if [ $uploadfile == y ] || [ $uploadfile == Y ]; then
 	if [ -z "$message" ]; then
 		git commit -m "A commit has been made."
 	else
