cb14776099011ec4ac6683ef0844d723619cd159
CSNguyenT <tommy.1115@revature.net>
2021-11-16 21:10:20 -0500

    makeloopfile.sh and upload.sh have been added to the repo.

---
+++ makeloopfile.sh
@@ -0,0 +1,15 @@
+mkdir ./loopresult
+touch ./loopresult/loopfile.txt
+echo "Results from makeloopfile.sh file" > ./loopresult/loopfile.txt
+for i in {1..20..2}
+do
+    echo "$i" >> ./loopresult/loopfile.txt
+done
+echo ""
+for i in {2..20..2}
+do
+    echo -n "$i" >> ./loopresult/loopfile.txt
+	if [ "$i" -lt "20" ]; then
+		echo -n ", " >> ./loopresult/loopfile.txt
+	fi
+done
---
+++ upload.sh
@@ -0,0 +1,49 @@
+# make file directory variable to read
+cwd=$(pwd)
+# variable for loop to add more files if user wants to
+addfile="Y"
+
+#this loop is for asking to add more files to push
+while [ "$addfile" = "y" ] || [ "$addfile" = "Y" ]; do
+	# --ask to select the file(s) in the working directory--
+	echo "Which file(s) should be uploaded in this directory?"
+	echo "Your current working directory: $cwd"
+	read file
+	
+	# --check if all files are selected or if no response was made and proceed if conditions are met--
+	if [ "${file: -1}" == "." ]; then
+		echo "Are you sure you want to upload all files in that directory? Type 'y' to allow."
+		read allow
+		if [ "$allow" == "y" ] || [ "$allow" == "Y" ]; then
+			git add "$file"
+		fi
+	elif [ -z "$file" ]; then
+		echo "There is no input."
+		allow="N"
+		
+	else
+		git add "$file"
+		allow="Y"
+	fi
+	
+	# allow to read if file has been added
+	if [ "$allow" == "y" ] || [ "$allow" == "Y" ]; then
+		echo "Attempted to add $file to the index."
+	fi
+	echo "Would you like to add another file? Type 'y' to add another file."
+	read addfile
+done
+
+#commit and push
+echo "Write a commit for the changes to be made."
+read message
+echo "type 'y' to push these files for change or type anything else to cancel."
+read uploadfile
+if [ "$uploadfile" == "y" ] || [ "$uploadfile" == "Y" ]; then
+	if [ -z "$message" ]; then
+		git commit -m "A commit has been made."
+	else
+		git commit -m "$message"
+	fi
+	git push
+fi
